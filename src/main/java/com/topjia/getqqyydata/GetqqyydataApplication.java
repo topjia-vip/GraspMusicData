c0b23d8a9786163fd1e70b09f7de6aed64d011c1
wjh <1256957450@qq.com>
2019-11-27 17:27:29 +0800

    提交

---
+++ src/main/java/com/topjia/getqqyydata/GetqqyydataApplication.java
@@ -0,0 +1,13 @@
+package com.topjia.getqqyydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetqqyydataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetqqyydataApplication.class, args);
+    }
+
+}
